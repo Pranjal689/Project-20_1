da597f3e467bcfdf7a1c78bf29305e79b63456b3
Pranjal689 <76397224+Pranjal689@users.noreply.github.com>
2020-12-29 10:04:26 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,6 +14,7 @@ function setup() {
 function draw() {
   background("black");  
   drawSprites();
+  console.log(deformation)
   if(wall.x-car.x<(car.width+wall.width)/2){
     car.velocityX=0;
     var deformation=0.5 * weight * speed* speed/22509;
